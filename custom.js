41bc98e1224346c8e6c26e5254f1ab5164efc3d0
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-03-14 14:53:28 +0900

    implemented header and content

--- custom.js
+++ custom.js
@@ -1,7 +1,8 @@
+// tab click change content
 $(".tab").on("click", () => {
     $(".is_active").removeClass("is_active");
     $(this).addClass("is_active");
     $(".is_show").removeClass("is_show");
     const className = $(this).attr("class");
-    $(".content").eq(idx).addClass("is_show");
+    $(".content_wrapper").eq(idx).addClass("is_show");
 });
